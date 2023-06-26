f2f28440285f6c447d69bad87d5cc0cce543d8f6
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-06-26 18:24:40 +0100

    1.0.1: Add images for the slider

---
+++ components/Slider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Slider = () => {
+  return (
+    <div className="m-auto">
+      <img src="/slider-1.png" />
+      <img src="/slider-2.png" />
+    </div>
+  );
+};
+
+export default Slider;
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Slider from "@components/Slider";
 
 const Hero = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <Slider />
+    </section>
+  );
 };
 
 export default Hero;
+
+// CREDIT FOR IMAGE USED
+// <a href="https://fr.freepik.com/vecteurs-libre/modele-banniere-horizontale-vente-ete-elements-plage-ete-parasol-appartements_15422371.htm#page=2&query=summer&position=12&from_view=search&track=sph">Image de user15245033</a> sur Freepik
